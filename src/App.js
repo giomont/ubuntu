564a150388edf42a6f6693643d96abbfa319f150
Giovanni Montoya <giomont@gmail.com>
2025-02-12 20:35:43 +0000

    335

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import MyPage from './components/MyPage';
+
+// ...existing code...
+
+function App() {
+  return (
+    <div className="App">
+      // ...existing code...
+      <MyPage />
+      // ...existing code...
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MyPage.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const MyPage = () => {
+    return (
+        <div>
+            <h1>Bienvenido a Mi Página</h1>
+            <p>Esta es una página React en tu repositorio.</p>
+        </div>
+    );
+};
+
+export default MyPage;
