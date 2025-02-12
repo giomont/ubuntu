c47f21528232d58c8f749de6dd3349e166fd37c9
Giovanni Montoya <giomont@gmail.com>
2025-02-12 20:55:08 +0000

    355

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import MyPage from './components/MyPage';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MyPage />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
