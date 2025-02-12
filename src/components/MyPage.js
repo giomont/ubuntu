55834ce0c710fc800e07903ef7e43c5d16f8504e
Giovanni Montoya <giomont@gmail.com>
2025-02-12 21:12:29 +0000

    412

--- src/components/MyPage.js
+++ src/components/MyPage.js
@@ -4,8 +4,8 @@ import React from 'react';
 const MyPage = () => {
     return (
         <div>
-            <h1>Bienvenido a Mi Página</h1>
-            <p>Esta es una página React en tu repositorio.</p>
+            <h1>UBUNTU </h1>
+            <p>conversa sabrosa.</p>
         </div>
     );
 };
